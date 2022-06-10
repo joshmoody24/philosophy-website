abc158baa1aa7ee623bcc5ddaa2c0e15d271a79e
Josh Moody <joshmoody24@gmail.comw>
2022-06-10 04:53:36 +0000

    got the outline of the site in place

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import moon from './img/moon.jpg';
 import { useRef } from 'react';
 
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import FlipCard from './components/FlipCard';
 
 function App() {
 
@@ -13,50 +15,108 @@ function App() {
 
   return (
     <div className="App">
-      <Parallax pages={4} ref={ref}>
+      <Parallax pages={100} ref={ref}>
 
         <ParallaxLayer
           offset={0}
-          speed={.05}
+          speed={1}
           factor={1}
           style={{
             backgroundImage: `url(${moon})`,
             backgroundSize: 'cover',
           }}
         >
-          <h2>Welcome to the website</h2>
-          <Button>Test</Button>
         </ParallaxLayer>
 
         <ParallaxLayer
-          offset={3.2}
-          speed={2}
+          offset={0}
+          speed={0.1}
+	  className="flex-center flex-column"
         >
-          <h2>Testing page</h2>
+          <h1 className="display-1">Philosophy of Life</h1>
         </ParallaxLayer>
 
         <ParallaxLayer
-          offset={0.5}
-          sticky={{ start: 0.9, end: 2.5 }}
+          offset={0.95}
+	  speed={-2}
           onClick={() => {
             console.log(ref);
             ref.current.scrollTo(3)
-            console.log("test")
           }}
         >
-          <h2>Sticky, click to scroll</h2>
+          <h2>By Josh Moody</h2>
         </ParallaxLayer>
 
         <ParallaxLayer
-          offset={2}
-          speed={1}
-          factor={4}
-          style={{
-            backgroundImage: `url(${moon})`,
-            backgroundSize: 'cover',
-          }}
+          sticky={{start: 3, end: 5.5}}
+	  className="p-5"
         >
-          <h2>Testing page</h2>
+	  <h1 className="display-1">The Good</h1>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+	  sticky={{start: 3.5, end: 6}}
+	  className="flex-center"
+	  style={{ width: "33.3%" }}
+        >
+	  <Card variant={"Dark"} bg={"dark"} style={{width:"100%", margin: "2rem"}}>
+	    <Card.Title>1</Card.Title>
+	    <Card.Body>
+	     Learn as much as possible
+	      <br />
+	      <i className="bi bi-book"></i>
+	    </Card.Body>
+	  </Card>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+	  sticky={{start: 4, end: 6.5}}
+	  className="flex-center"
+	  style={{ width: "33.3%", marginLeft: "33.3%" }}
+        >
+	  <Card variant={"Dark"} bg={"dark"} style={{width:"100%", margin: "2rem"}}>
+	    <Card.Title>2</Card.Title>
+	    <Card.Body>
+	      Develop self-mastery
+	      <br />
+	      <i className="bi bi-person-check"></i>
+	    </Card.Body>
+	  </Card>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+	  sticky={{start: 4.5, end: 7}}
+	  className="flex-center"
+	  style={{ width: "33.3%", marginLeft: "66.6%" }}
+        >
+	  <Card variant={"Dark"} bg={"dark"} style={{width:"100%", margin: "2rem"}}>
+	    <Card.Title>3</Card.Title>
+	    <Card.Body>
+	      Help as many people as possible do this
+	      <br />
+	      <i className="bi bi-arrow-left"></i>
+	    </Card.Body>
+	  </Card>
+        </ParallaxLayer>
+
+	<ParallaxLayer
+	  offset={8}
+	>
+	 <FlipCard body="test card" title="test title" /> 
+	</ParallaxLayer>
+
+        <ParallaxLayer
+	  sticky={{start: 9, end: 12}}
+	  className="flex-center"
+        >
+	  <Card variant={"Dark"} bg={"dark"} style={{width:"100%", margin: "2rem"}}>
+	    <Card.Title>3</Card.Title>
+	    <Card.Body>
+	      Help as many people as possible do this
+	      <br />
+	      <i className="bi bi-arrow-left"></i>
+	    </Card.Body>
+	  </Card>
         </ParallaxLayer>
 
       </Parallax>
---
+++ src/components/FlipCard.js
@@ -0,0 +1,69 @@
+import Card from 'react-bootstrap/Card';
+import { useState } from 'react';
+
+export default function FlipCard({
+	title = "Card Title",
+	body = "Card Body",
+	backTitle = "Back Title",
+	backBody = "Back Body",
+	transition = 0.5,
+	direction="Y",
+	zIndex = 0,
+	height = 300,
+	width = 300,
+}
+) {	
+
+	const [flipped, setFlipped] = useState(false);
+	
+	function flipOver(){
+		setFlipped(!flipped);
+	}
+
+	function showFront(){
+		setFlipped(true)
+	}
+
+	function showBack(){
+		setFlipped(false)
+	}
+
+	function calcDegrees(flipped){
+		return flipped ? 0 : 180;
+	}
+
+	return (
+	<div style={{position:"relative", width: width, height: height, display: "flex", margin: "2rem"}}
+		onMouseEnter={() => showFront()}
+		onMouseLeave={() => showBack()}
+	>
+	  <Card 
+		variant={"Dark"} bg={"dark"}
+		className="flex-center"
+		style={{
+			width:"100%", height:"100%", zIndex:zIndex,
+			transition: transition + "s",
+			backfaceVisibility: "hidden",
+			position: "absolute",
+			transform: `rotate${direction}(${calcDegrees(flipped)}deg)`}}
+	  >
+	    <Card.Title>{title}</Card.Title>
+		{body}
+	  </Card>
+
+	  <Card 
+		variant={"Dark"} bg={"dark"}
+		className="flex-center"
+		style={{
+			width:"100%", height:"100%", zIndex:zIndex,
+			transition: transition + "s",
+			backfaceVisibility: "hidden",
+			position: "absolute",
+			transform: `rotate${direction}(${calcDegrees(!flipped)}deg)`}}
+	  >
+	    <Card.Title>{backTitle}</Card.Title>
+		{backBody}
+	  </Card>
+	</div>
+	)
+}
